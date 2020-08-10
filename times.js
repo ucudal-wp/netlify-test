0fc4f92910ab649d4eca8b844c2ac1552d59b5f5
Matías Olivera <moliverafreire@gmail.com>
2020-08-10 20:22:46 -0300

    feat: add multiply function (#5)

---
+++ times.js
@@ -0,0 +1,6 @@
+function times(a, b) {
+  // Multiply a * b.
+  return a * b;
+}
+
+module.exports = times;
---
+++ times.test.js
@@ -0,0 +1,5 @@
+const times = require('./times');
+
+test('multiplies 1 * 2 to equal 2', () => {
+  expect(times(1, 2)).toBe(2);
+});
