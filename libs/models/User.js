c4382bcccea9394f863289a03c673d67ccf02df4
Matias Olivera <moliverafreire@gmail.com>
2020-09-23 18:44:57 -0300

    feat: add User model

---
+++ libs/models/User.js
@@ -0,0 +1,51 @@
+const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
+
+const saltWorkFactor = 10;
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: { type: String, required: true },
+  firstName: { type: String, required: true, trim: true },
+  lastName: { type: String, required: true, trim: true },
+});
+
+userSchema.virtual('fullName').get(function getFullName() {
+  return `${this.firstName} ${this.lastName}`;
+});
+
+/**
+ * Compare the given plain-text password with the user's password.
+ *
+ * @param {string} password A plain-text password.
+ *
+ * @return {boolean} Whether the password matches or not.
+ */
+userSchema.methods.comparePassword = function comparePassword(password) {
+  return bcrypt.compare(password, this.password);
+};
+
+/**
+ * Hash the user's password.
+ */
+async function hashPassword() {
+  const user = this;
+
+  if (!user.password || !user.isModified('password')) {
+    return;
+  }
+
+  const salt = await bcrypt.genSalt(saltWorkFactor);
+  user.password = await bcrypt.hash(user.password, salt);
+}
+userSchema.pre('save', hashPassword);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
---
+++ libs/models/User.test.js
@@ -0,0 +1,130 @@
+const mongoose = require('mongoose');
+
+const User = require('./User');
+
+describe('User', () => {
+  let user;
+
+  beforeEach(() => {
+    user = new User({
+      firstName: 'Han',
+      lastName: 'Solo',
+      email: 'millenniumfalcon@mail.com',
+      password: 'NeverTellMeTheOdds',
+    });
+  });
+
+  test('is saved successfully', async () => {
+    const savedUser = await user.save();
+    expect(savedUser).toMatchObject(user);
+  });
+
+  describe('fields', () => {
+    describe('email', () => {
+      test('is converted to lowercase before saving', async () => {
+        const uppercaseEmail = 'MILLENNIUMFALCON@MAIL.COM';
+        user.email = uppercaseEmail;
+        const savedUser = await user.save();
+        expect(savedUser.email).toBe(uppercaseEmail.toLowerCase());
+      });
+
+      test("doesn't allow duplicates", async () => {
+        expect.assertions(2);
+
+        await user.save();
+
+        const user2 = new User({
+          firstName: 'Lando',
+          lastName: 'Calrissian',
+          email: 'millenniumfalcon@mail.com',
+          password: 'iMissTheFalcon',
+        });
+        try {
+          await user2.save();
+        } catch (err) {
+          expect(err.name).toBe('MongoError');
+          expect(err.code).toBe(11000);
+        }
+      });
+    });
+
+    describe('password', () => {
+      test('is hashed before saving', async () => {
+        const plaintextPassword = user.password;
+        const savedUser = await user.save();
+        expect(savedUser.password).not.toBe(plaintextPassword);
+      });
+
+      test('matches when comparing to original plain-text password', async () => {
+        const plaintextPassword = user.password;
+        const savedUser = await user.save();
+        const matches = await savedUser.comparePassword(plaintextPassword);
+        expect(matches).toBe(true);
+      });
+
+      test("doesn't match when comparing to wrong plain-text password", async () => {
+        const savedUser = await user.save();
+        const matches = await savedUser.comparePassword('wrong');
+        expect(matches).toBe(false);
+      });
+    });
+
+    test('has a fullName virtual field', () => {
+      expect(user.fullName).toBe('Han Solo');
+    });
+  });
+
+  describe('validation', () => {
+    test('passes if all required fields are present', async () => {
+      await user.validate();
+    });
+
+    test('fails if email is empty', async () => {
+      expect.assertions(2);
+
+      user.email = undefined;
+      try {
+        await user.validate();
+      } catch (err) {
+        expect(err).toBeInstanceOf(mongoose.Error.ValidationError);
+        expect(err.errors.email).toBeDefined();
+      }
+    });
+
+    test('fails if password is empty', async () => {
+      expect.assertions(2);
+
+      user.password = undefined;
+      try {
+        await user.validate();
+      } catch (err) {
+        expect(err).toBeInstanceOf(mongoose.Error.ValidationError);
+        expect(err.errors.password).toBeDefined();
+      }
+    });
+
+    test('fails if firstName is empty', async () => {
+      expect.assertions(2);
+
+      user.firstName = undefined;
+      try {
+        await user.validate();
+      } catch (err) {
+        expect(err).toBeInstanceOf(mongoose.Error.ValidationError);
+        expect(err.errors.firstName).toBeDefined();
+      }
+    });
+
+    test('fails if lastName is empty', async () => {
+      expect.assertions(2);
+
+      user.lastName = undefined;
+      try {
+        await user.validate();
+      } catch (err) {
+        expect(err).toBeInstanceOf(mongoose.Error.ValidationError);
+        expect(err.errors.lastName).toBeDefined();
+      }
+    });
+  });
+});
