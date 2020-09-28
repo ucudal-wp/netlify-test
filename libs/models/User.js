ef41c6a0ef8fa1e9125c46676b07be4d55663235
Matias Olivera <moliverafreire@gmail.com>
2020-09-28 19:57:32 -0300

    feat: replace User email field with username

--- libs/models/User.js
+++ libs/models/User.js
@@ -4,13 +4,7 @@ const mongoose = require('mongoose');
 const saltWorkFactor = 10;
 
 const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-  },
+  username: { type: String, required: true, unique: true, trim: true },
   password: { type: String, required: true },
   firstName: { type: String, required: true, trim: true },
   lastName: { type: String, required: true, trim: true },
