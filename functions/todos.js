19f1acc56dd85f0cf2613ef58b060a8912faa872
Matias Olivera <moliverafreire@gmail.com>
2020-09-28 19:41:00 -0300

    feat: fetch todos from database

--- functions/todos.js
+++ functions/todos.js
@@ -1,15 +1,14 @@
-const todos = [
-  {
-    id: '123',
-    text: 'Foo bar',
-  },
-  {
-    id: '124',
-    text: 'Bar baz',
-  },
-];
+const { Todo } = require('../libs/models');
+
+const { mongodb } = require('../libs/connectors');
+
+const mongodbUri = process.env.MONGODB_URI;
 
 exports.handler = async () => {
+  await mongodb(mongodbUri);
+
+  const todos = await Todo.find();
+
   return {
     statusCode: 200,
     headers: {
