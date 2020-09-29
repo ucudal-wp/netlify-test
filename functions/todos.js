d72ac586294362029decaf1155fe2f4bd0548c92
Matias Olivera <moliverafreire@gmail.com>
2020-09-28 21:02:23 -0300

    feat: check authentication for todos service

--- functions/todos.js
+++ functions/todos.js
@@ -1,11 +1,80 @@
-const { Todo } = require('../libs/models');
+const jwt = require('jsonwebtoken');
 
+const { Todo, User } = require('../libs/models');
 const { mongodb } = require('../libs/connectors');
 
+const jwtSecret = process.env.JWT_SECRET;
 const mongodbUri = process.env.MONGODB_URI;
+
+const verifyToken = (token) => {
+  try {
+    return jwt.verify(token, jwtSecret);
+  } catch (err) {
+    if (err.name === 'JsonWebTokenError') {
+      return null;
+    }
+
+    throw err;
+  }
+};
 
 exports.handler = async (event) => {
   await mongodb(mongodbUri);
+
+  const { body, headers } = event;
+
+  const { authorization } = headers;
+  if (!authorization) {
+    return {
+      statusCode: 401,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ error: 'You must be logged in.' }),
+    };
+  }
+
+  const [scheme, token] = authorization.split(' ');
+
+  if (!/^Bearer$/i.test(scheme)) {
+    return {
+      statusCode: 401,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        error: `Unsupported authentication scheme: "${scheme}". Supported schemes: "Bearer".`,
+      }),
+    };
+  }
+
+  const decoded = verifyToken(token);
+  if (!decoded) {
+    return {
+      statusCode: 401,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        error: 'Invalid token.',
+      }),
+    };
+  }
+
+  const { sub: userId } = decoded;
+  const user = await User.findById(userId);
+
+  if (!user) {
+    return {
+      statusCode: 401,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        error: 'Invalid token.',
+      }),
+    };
+  }
 
   if (event.httpMethod === 'GET') {
     const todos = await Todo.find();
@@ -20,8 +89,6 @@ exports.handler = async (event) => {
   }
 
   if (event.httpMethod === 'POST') {
-    const { body } = event;
-
     const { text } = JSON.parse(body);
 
     const todo = new Todo({ text });
